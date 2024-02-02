d64f483787520993e05a03d90dc8f1c3a580e0f9
Johan Beimers <35890334+JohanOnPc@users.noreply.github.com>
2024-02-02 01:56:23 +0100

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "Please specify a file to analyze!\n";
+		return -1;
+	}
+
+	HANDLE file = CreateFileA(argv[1], GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (file == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "CreateFile failed with error: " << GetLastError() << "\n";
+		return -2;
+	}
+
+	HANDLE fileMapping = CreateFileMappingA(file, nullptr, PAGE_READONLY, 0, 0, nullptr);
+
+	if (fileMapping == NULL)
+	{
+		std::cout << "CreateFileMapping failed with error: " << GetLastError() << "\n";
+		return -3;
+	}
+
+	std::byte* map = reinterpret_cast<std::byte*>(MapViewOfFile(fileMapping, FILE_MAP_READ, 0, 0, 0));
+
+	if (map == nullptr)
+	{
+		std::cout << "MapViewOfFile failed with error: " << GetLastError() << "\n";
+		return -4;
+	}
+
+	std::cout << char(map[0]) << char(map[1]) << "\n";
+
+	UnmapViewOfFile(map);
+	CloseHandle(fileMapping);
+	CloseHandle(file);
+
+	return 0;
+}
